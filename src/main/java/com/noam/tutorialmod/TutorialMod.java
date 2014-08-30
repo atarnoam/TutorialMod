365d8537ff466bef032fff776c27fd88ab211211
Noam <atarnoam@gmail.com>
2014-08-30 23:51:52 +0300

    lololol

---
+++ src/main/java/com/noam/tutorialmod/TutorialMod.java
@@ -0,0 +1,41 @@
+package com.noam.tutorialmod;
+
+import com.noam.tutorialmod.handler.ConfigurationHandler;
+import com.noam.tutorialmod.reference.*;
+import com.noam.tutorialmod.utility.LogHelper;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import com.noam.tutorialmod.proxy.*;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
+
+public class TutorialMod
+{
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    public static IProxy proxy;
+
+    @Mod.Instance(Reference.MOD_ID)
+    public static TutorialMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        LogHelper.info("Pre Init Complete!");
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        LogHelper.info("Init Complete!");
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        LogHelper.info("Post Init Complete!");
+    }
+}
