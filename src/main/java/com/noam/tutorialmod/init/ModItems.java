8b4d561a34653eb9a0fd9f8853e2656f79764831
Noam <atarnoam@gmail.com>
2014-08-31 14:35:07 +0300

    changes

--- src/main/java/com/noam/tutorialmod/init/ModItems.java
+++ src/main/java/com/noam/tutorialmod/init/ModItems.java
@@ -1,8 +1,10 @@
 package com.noam.tutorialmod.init;
 
 import com.noam.tutorialmod.item.*;
+import com.noam.tutorialmod.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 
+@GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModItems
 {
     public static final ItemTM cheeseburger = new ItemCheeseburger();
