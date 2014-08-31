506770cde274b1280b80cf10d48727d0d464f321
Noam <atarnoam@gmail.com>
2014-08-31 14:35:00 +0300

    changes

--- src/main/java/com/noam/tutorialmod/init/ModBlocks.java
+++ src/main/java/com/noam/tutorialmod/init/ModBlocks.java
@@ -1,8 +1,10 @@
 package com.noam.tutorialmod.init;
 
 import com.noam.tutorialmod.block.*;
+import com.noam.tutorialmod.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 
+@GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModBlocks
 {
     public static final BlockTM PathLight = new BlockPathLight();
